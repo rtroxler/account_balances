7a6cd089b72a8273df19ec8a5a12b8c2ce2e5881
Ryan Troxler <rtroxler@rednovalabs.com>
2017-11-18 14:18:45 -0600

    Split Txns and assessments up, impl AccountBalance

--- src/main.rs
+++ src/main.rs
@@ -4,25 +4,32 @@ use chrono::prelude::*;
 mod usd;
 use usd::USD;
 
-
 #[derive(Debug)]
-struct AccountBalance {
+struct Transaction {
+    assessment_id: i64,
     amount: USD,
     account_code: String,
     effective_on: DateTime<Utc>,
-    service_start_date: DateTime<Utc>,
-    service_end_date: DateTime<Utc>,
-    modifier: String,
 }
 
-impl AccountBalance {
+#[derive(Debug)]
+struct Assessment {
+    id: i64,
+    amount: USD,
+    account_code: String,
+    effective_on: DateTime<Utc>,
+    service_start_date: Option<DateTime<Utc>>,
+    service_end_date: Option<DateTime<Utc>>,
+}
+
+impl Assessment {
     fn days_in_service_period(&self) -> u64 {
-        let duration = self.service_end_date.signed_duration_since(self.service_start_date);
+        let duration = self.service_end_date.unwrap().signed_duration_since(self.service_start_date.unwrap());
         (duration.to_std().unwrap().as_secs() / 86_400) + 1
     }
 
     fn amount_per_day(&self) -> Vec<(DateTime<Utc>, USD)> {
-        // TODO: Worry about negative numbers?
+        // TODO: Worry about negative numbers at some point?
         let spd = self.amount.pennies / self.days_in_service_period();
         let mut leftover = self.amount.pennies % self.days_in_service_period();
 
@@ -32,27 +39,60 @@ impl AccountBalance {
                 day_amount += 1;
                 leftover -= 1;
             }
-            (self.service_start_date + chrono::Duration::days(day as i64), USD::from_pennies(day_amount) )
+            (self.service_start_date.unwrap() + chrono::Duration::days(day as i64),
+             USD::from_pennies(day_amount) )
         }).collect()
     }
 }
 
+trait AccountBalance {
+    fn process(&self);
+}
+impl AccountBalance for Transaction {
+    fn process(&self) {
+        // We're a payment, pay for things
+        println!("\tHere's a payment!");
+        println!("Processing {:?}\n", self);
+    }
+}
+impl AccountBalance for Assessment {
+    fn process(&self) {
+        // We're assessment (charge), write entries based on our account code
+        println!("\tHere's our amount per day:");
+        println!("{:?}\n", self.amount_per_day());
+    }
+}
+
+fn process(account_balances: Vec<Box<AccountBalance>>) {
+    for ab in account_balances.iter() {
+        ab.process();
+    }
+}
+
 fn main() {
-    let rent_charge = AccountBalance {
-        amount: USD::from_float(33.01),
-        account_code: String::from("1101"),
+    let rent_charge = Assessment {
+        id: 1,
+        amount: USD::from_float(30.0),
+        account_code: String::from("4000"),
         effective_on: Utc.ymd(2017, 11, 1).and_hms(0,0,0),
-        service_start_date: Utc.ymd(2017, 11, 1).and_hms(0,0,0),
-        service_end_date: Utc.ymd(2017, 11, 30).and_hms(0,0,0),
-        modifier: String::from("rent")
+        service_start_date: Some(Utc.ymd(2017, 11, 1).and_hms(0,0,0)),
+        service_end_date: Some(Utc.ymd(2017, 11, 30).and_hms(0,0,0)),
     };
 
-    println!("{:?}", rent_charge);
-    println!("amount: {:?}", rent_charge.amount);
+    let payment = Transaction {
+        assessment_id: 1,
+        amount: USD::from_float(30.0),
+        account_code: String::from("1000"),
+        effective_on: Utc.ymd(2017, 11, 1).and_hms(0,0,0),
+    };
 
-    println!("{:?} days in service period\n", rent_charge.days_in_service_period());
-
-    // Next:
+    //// Next:
     // Daily accrual it!
-    println!("{:?}", rent_charge.amount_per_day());
+    //println!("{:?}", rent_charge.amount_per_day());
+
+    let mut account_balances: Vec<Box<AccountBalance>> = Vec::new(); //vec!(rent_charge, payment);
+    account_balances.push(Box::new(rent_charge));
+    account_balances.push(Box::new(payment));
+    process(account_balances);
 }
+
