a8923e0e402c72ab8e79a31ee29b14063b8d2e73
Ryan Troxler <rtroxler@rednovalabs.com>
2018-01-21 16:36:30 -0600

    Add unit test for rent and fee assessments
    Fee is obviously failing since I don't have an account code mapper

--- src/main.rs
+++ src/main.rs
@@ -224,3 +224,48 @@ fn main() {
     //general_ledger::generate();
 }
 
+#[test]
+fn test_rent_account_balance_accrues_daily() {
+    let rent_charge = Assessment {
+        amount: USD::from_float(30.0),
+        account_code: String::from("4000"),
+        effective_on: Utc.ymd(2017, 11, 1).and_hms(0,0,0),
+        service_start_date: Some(Utc.ymd(2017, 11, 1).and_hms(0,0,0)),
+        service_end_date: Some(Utc.ymd(2017, 11, 30).and_hms(0,0,0)),
+    };
+
+    let mut gl = GeneralLedger::new();
+    rent_charge.process(&mut gl);
+    let start = rent_charge.service_start_date.unwrap().date();
+    let end = rent_charge.service_end_date.unwrap().date();
+
+    let mut date_stepper = start;
+    while date_stepper <= end {
+        assert_eq!(gl.fetch_amount(date_stepper, String::from("1101")), Some(&USD::from_float(1.0)));
+        assert_eq!(gl.fetch_amount(date_stepper, String::from("4000")), Some(&USD::from_float(-1.0)));
+
+        date_stepper = date_stepper.checked_add_signed(chrono::Duration::days(1))
+            .expect("Overflow");
+    }
+}
+
+#[test]
+fn test_fee_account_balance_accrues_periodically() {
+    let fee_charge = Assessment {
+        amount: USD::from_float(30.0),
+        account_code: String::from("4050"), // Fee
+        effective_on: Utc.ymd(2017, 11, 1).and_hms(0,0,0),
+        service_start_date: None,
+        service_end_date: None,
+    };
+
+    let mut gl = GeneralLedger::new();
+    fee_charge.process(&mut gl);
+
+    assert_eq!(gl.fetch_amount(fee_charge.effective_on.date(), String::from("1103")), Some(&USD::from_float(30.0)));
+    assert_eq!(gl.fetch_amount(fee_charge.effective_on.date(), String::from("4050")), Some(&USD::from_float(-30.0)));
+
+    // Doesn't have anything the next day
+    // assert entries count == 2
+}
+
